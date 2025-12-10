5678432fbfbf4aa2a047907059393a65857f57bb
Neil Perry <142518199+npv0@users.noreply.github.com>
2025-12-10 16:16:58 +0000

    Initial commit

---
+++ src/main/modules/appinsights/index.ts
@@ -0,0 +1,17 @@
+import config from 'config';
+
+const appInsights = require('applicationinsights');
+
+export class AppInsights {
+  enable(): void {
+    if (config.get('appInsights.instrumentationKey')) {
+      appInsights.setup(config.get('app-insights-connection-string')).setSendLiveMetrics(true).start();
+
+      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] =
+        'juror-er-portal';
+      appInsights.defaultClient.trackTrace({
+        message: 'App insights activated',
+      });
+    }
+  }
+}
