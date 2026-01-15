6e2d4a1719e835c2358abe5f5b56a7f3320b549a
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-01-15 13:51:19 +0000

    Add quick post route for dev login

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,7 +1,18 @@
 import { Application } from 'express';
 
+const { Logger } = require('@hmcts/nodejs-logging');
+
+const logger = Logger.getLogger('app');
+
 export default function (app: Application): void {
   app.get('/', (req, res) => {
-    res.render('login');
+    res.render('login', {
+      devLoginUrl: '/dev-login',
+    });
+  });
+
+  app.post('/dev-login', (req, res) => {
+    logger.info(`Dev login with user: ${req.body.email}`);
+    return res.redirect('/');
   });
 }
