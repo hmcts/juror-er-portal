470e214f546346eb5dcb4df94c9a25f017ce8458
Neil Perry <neil.perry@cgi.com>
2026-01-16 15:13:29 +0000

    update name to match rules

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -2,7 +2,7 @@ locals {
   resourceName = "${var.product}-${var.env}-rg"
   vaultName    = "${var.product}-${var.env}"
   cacheName    = "${var.product}-${var.component}-redis-cache"
-  storageName  = "${var.product}-${var.component}"
+  storageName  = "${var.product}sa${var.env}"
 
   private_endpoint_rg_name   = "ss-${var.env}-network-rg"
   private_endpoint_vnet_name = "ss-${var.env}-vnet"
