6467c8e606df759244cec2762fa62f242e4668c6
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-01-16 11:57:20 +0000

    Add initial redis support

--- src/main/modules/session/index.ts
+++ src/main/modules/session/index.ts
@@ -1,33 +1,81 @@
 import secretsConfig from 'config';
+import { RedisStore } from 'connect-redis';
 import * as express from 'express';
-
-const expressSession = require('express-session');
+import session, { SessionOptions } from 'express-session';
+import { RedisClientType, createClient } from 'redis';
 
 export class SessionConfig {
-  private _sessionExpires: number;
-
-  constructor() {
-    this._sessionExpires = 10 * (60 * 60);
-  }
+  private _sessionExpires: number = 10 * 60 * 60; // seconds
+  private _redisClient?: RedisClientType;
 
   public start(app: express.Express): void {
     const secret: string = secretsConfig.get('secrets.juror.er-portal-sessionSecret');
+    
+    const redisConnectionString = this._getRedisConnectionString();
+    
     const config = this._config(secret);
-    app.use(expressSession(config));
+
+    if (redisConnectionString) {
+      this.redisClient();
+      config.store = this.redisStore();
+    }
+
+    app.use(session(config));
   }
 
-  private _config(secret: string) {
+  private redisClient() {
+    this._redisClient = createClient({
+      url: secretsConfig.get('secrets.juror.er-portal-redisConnection'),
+      pingInterval: 5000,
+      socket: {
+        keepAlive: true,
+      },
+    });
+
+    this._redisClient.connect()
+      .catch((error: unknown) => {
+        console.log('Error connecting redis client: ', error);
+      });
+
+    this._redisClient.on('error', (err: unknown) => {
+      console.log(new Date().toLocaleString() + ' - ' + 'Could not connect to redis ' + String(err));
+    });
+    this._redisClient.on('connect', () => {
+      console.log(new Date().toLocaleString() + ' - ' + 'Connected to redis successfully');
+    });
+  }
+
+  private redisStore() {
+    return new RedisStore({
+      client: this._redisClient,
+      prefix: 'JurorERPortal:',
+    });
+  }
+
+  private _getRedisConnectionString() {
+    let redisConnectionString;
+
+    try {
+      redisConnectionString = secretsConfig.get('secrets.juror.er-portal-redisConnection');
+      return redisConnectionString;
+    } catch (err) {
+      console.log('Redis connection string is not available... setting in memory sessions');
+      return;
+    }
+  }
+
+  private _config(secret: string): SessionOptions {
     const isProduction = process.env.NODE_ENV === 'production';
     return {
       secret,
       resave: false,
       saveUninitialized: false,
-      maxAge: this._sessionExpires,
       name: 'juror_er_portal_session',
       cookie: {
         secure: isProduction,
         sameSite: 'lax' as const, // oauth redirect does not work with strict
         httpOnly: true,
+        maxAge: this._sessionExpires * 1000,
       },
     };
   }
