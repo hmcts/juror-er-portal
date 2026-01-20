746ef359791f6d0c9a817376023dd4133956f0be
Neil Perry <neil.perry@cgi.com>
2026-01-20 17:07:50 +0000

    update app insights check

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -4,7 +4,7 @@ const appInsights = require('applicationinsights');
 
 export class AppInsights {
   enable(): void {
-    if (config.get('appInsights.instrumentationKey')) {
+    if (config.get('secrets.juror.app-insights-connection-string')) {
       appInsights.setup(config.get('secrets.juror.app-insights-connection-string')).setSendLiveMetrics(true).start();
 
       appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'juror-er-portal';
