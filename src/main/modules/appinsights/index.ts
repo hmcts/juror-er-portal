e22569aee13fa1fd27a424922a29cc2ff9d28864
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-01-20 13:48:25 +0000

    Update development config

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -5,7 +5,7 @@ const appInsights = require('applicationinsights');
 export class AppInsights {
   enable(): void {
     if (config.get('appInsights.instrumentationKey')) {
-      appInsights.setup(config.get('app-insights-connection-string')).setSendLiveMetrics(true).start();
+      appInsights.setup(config.get('secrets.juror.app-insights-connection-string')).setSendLiveMetrics(true).start();
 
       appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'juror-er-portal';
       appInsights.defaultClient.trackTrace({
