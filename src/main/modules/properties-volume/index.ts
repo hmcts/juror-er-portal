11d7673e1500059fbd9e4daed37af6317c6623d9
Neil Perry <neil.perry@cgi.com>
2026-01-21 14:42:28 +0000

    remove setSecret

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -1,18 +1,11 @@
 import * as propertiesVolume from '@hmcts/properties-volume';
 import config from 'config';
 import { Application } from 'express';
-import { get, set } from 'lodash';
 
 export class PropertiesVolume {
   enableFor(server: Application): void {
     if (server.locals.ENV !== 'development') {
       propertiesVolume.addTo(config);
-    }
-  }
-
-  private setSecret(fromPath: string, toPath: string): void {
-    if (config.has(fromPath)) {
-      set(config, toPath, get(config, fromPath));
     }
   }
 }
