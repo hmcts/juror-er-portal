c850c003ea846a2492215a770675d779802d237d
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-04-07 15:05:42 +0100

    Let app insights collect console statements

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -10,7 +10,7 @@ export class AppInsights {
       return;
     }
 
-    appInsights.setup(config.get('secrets.juror.app-insights-connection-string')).setSendLiveMetrics(true).start();
+    appInsights.setup(connectionString).setAutoCollectConsole(true, true).setSendLiveMetrics(true).start();
 
     appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'juror-er-portal';
     appInsights.defaultClient.trackTrace({
