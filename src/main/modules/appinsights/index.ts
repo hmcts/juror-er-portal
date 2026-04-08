e623012da7d339a44d19a4fd4b239605df3675d4
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-04-08 08:10:46 +0100

    Add extra app insights logging

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -5,12 +5,19 @@ const appInsights = require('applicationinsights');
 export class AppInsights {
   enable(): void {
     if (config.get('secrets.juror.app-insights-connection-string')) {
-      appInsights.setup(config.get('secrets.juror.app-insights-connection-string')).setSendLiveMetrics(true).start();
+      console.log('Enabling app insights');
+      appInsights
+        .setup(config.get('secrets.juror.app-insights-connection-string'))
+        .setAutoCollectConsole(true, true)
+        .setSendLiveMetrics(true)
+        .start();
 
       appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'juror-er-portal';
       appInsights.defaultClient.trackTrace({
         message: 'App insights activated',
       });
+    } else {
+      console.log('App insights connection string not found, app insights will not be enabled');
     }
   }
 }
