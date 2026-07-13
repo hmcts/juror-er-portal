bb52a75af683d309b7fbcb59e0859efc4d99c983
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-07-13 10:02:55 +0100

    Update health and info endpoints

--- src/main/routes/info.ts
+++ src/main/routes/info.ts
@@ -1,7 +1,9 @@
 import * as os from 'os';
 
+import { app as myApp } from '../app';
 import { infoRequestHandler } from '@hmcts/info-provider';
 import { Router } from 'express';
+import pkg from '../../../package.json';
 
 export default function (app: Router): void {
   app.get(
@@ -9,12 +11,12 @@ export default function (app: Router): void {
     infoRequestHandler({
       extraBuildInfo: {
         host: os.hostname(),
-        name: 'expressjs-template',
+        name: pkg.name,
+        version: pkg.version,
+        environment: myApp.locals.ENV,
         uptime: process.uptime(),
       },
-      info: {
-        // TODO: add downstream info endpoints if your app has any
-      },
+      info: {},
     })
   );
 }
